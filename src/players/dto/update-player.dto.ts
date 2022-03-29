e0bbd3748466b6d675e3c6b0947136c4cf6728ae
Abel Junior <abel.costa@webjump.com.br>
2022-03-29 15:11:11 -0300

    refactor(players): validation
    adding validation pipes

--- src/players/dto/update-player.dto.ts
+++ src/players/dto/update-player.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsEmail, IsPhoneNumber } from 'class-validator';
 import { CreatePlayerDto } from './create-player.dto';
 
-export class UpdatePlayerDto extends PartialType(CreatePlayerDto) {}
+export class UpdatePlayerDto extends PartialType(CreatePlayerDto) {
+  @IsEmail()
+  email?: string;
+
+  @IsPhoneNumber()
+  phone_number?: string;
+}
