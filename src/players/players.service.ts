e8d232d145dbea8eda98134d2dfee2482988593a
Abel Junior <abel.costa@webjump.com.br>
2022-03-29 00:24:31 -0300

    refactor(players): create
    not allows to create an player with an email already taken

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -11,7 +11,18 @@ export class PlayersService {
     @InjectModel('Player') private readonly playerModel: Model<Player>,
   ) {}
 
+  private async findByEmail(email: string): Promise<Player> {
+    const player = await this.playerModel.findOne({ email });
+
+    return player;
+  }
+
   async create({ name, email, phone_number }: CreatePlayerDto): Promise<void> {
+    const emailAlreadyTaken = this.findByEmail(email);
+
+    if (emailAlreadyTaken)
+      throw new HttpException('Email already taken', HttpStatus.CONFLICT);
+
     await this.playerModel.create({ name, email, phone_number });
   }
 
