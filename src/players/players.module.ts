0a419db5213a4c2e4bf536d3553b9614be8f6cb4
Abel Junior <abel.costa@webjump.com.br>
2022-09-12 15:08:03 -0300

    refactor(players): module
    registering repository for module

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -3,12 +3,13 @@ import { PlayersService } from './players.service';
 import { PlayersController } from './players.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Player, PlayerSchema } from './entities/player.entity';
+import { PlayersRepository } from './domain/repositories/players.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Player.name, schema: PlayerSchema }]),
   ],
   controllers: [PlayersController],
-  providers: [PlayersService],
+  providers: [PlayersService, PlayersRepository],
 })
 export class PlayersModule {}
