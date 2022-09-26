a85883237395967afe2a2fe4418d60a49737ec7b
junior <abelsouzacosta@gmail.com>
2022-09-26 03:01:11 -0300

    feat(players): pipe
    add a pipe to format players name

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -11,13 +11,14 @@ import {
 import { PlayersService } from './players.service';
 import { CreatePlayerDto } from './domain/dto/create-player.dto';
 import { UpdatePlayerDto } from './domain/dto/update-player.dto';
+import { FormattedNamePipePipe } from './pipes/formatted-name-pipe.pipe';
 
 @Controller('players')
 export class PlayersController {
   constructor(private readonly playersService: PlayersService) {}
 
   @Post()
-  @UsePipes(new ValidationPipe())
+  @UsePipes(new ValidationPipe(), new FormattedNamePipePipe())
   create(@Body() data: CreatePlayerDto) {
     return this.playersService.create(data);
   }
