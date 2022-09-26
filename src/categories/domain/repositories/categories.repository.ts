aec3741ff86b78d02eea9fecf08b8015e888ec44
Abel Junior <abel.costa@webjump.com.br>
2022-09-26 17:37:46 -0300

    refactor(categories): repository
    remove methods that are not responsability of the repository

--- src/categories/domain/repositories/categories.repository.ts
+++ src/categories/domain/repositories/categories.repository.ts
@@ -2,7 +2,6 @@ import { Category } from 'src/categories/entities/category.entity';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateCategoryDto } from '../dto/create-category.dto';
-import { HttpException, HttpStatus } from '@nestjs/common';
 import { UpdateCategoryDto } from '../dto/update-category.dto';
 import { UpdateResult } from 'mongodb';
 
@@ -12,21 +11,7 @@ export class CategoriesRepository {
     private readonly model: Model<Category>,
   ) {}
 
-  async throwsExceptionIfCategoryIsAlreadyTaken(
-    category: string,
-  ): Promise<void> {
-    const categoryFound = await this.findByCategory(category);
-
-    if (categoryFound)
-      throw new HttpException(
-        `Category ${category} Already Exists`,
-        HttpStatus.CONFLICT,
-      );
-  }
-
   async create(data: CreateCategoryDto): Promise<Category> {
-    await this.throwsExceptionIfCategoryIsAlreadyTaken(data.category);
-
     return this.model.create({
       ...data,
     });
