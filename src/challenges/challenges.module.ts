7effaf0de30be2a03939f3e578370ac8f11f9691
junior <abelsouzacosta@gmail.com>
2022-10-01 14:12:01 -0300

    refactor(challenge): module
    importing players module

--- src/challenges/challenges.module.ts
+++ src/challenges/challenges.module.ts
@@ -4,12 +4,14 @@ import { ChallengesController } from './challenges.controller';
 import { ChallengesRepository } from './domain/repository/challenges.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Challenge, ChallengeSchema } from './entities/challenge.entity';
+import { PlayersModule } from 'src/players/players.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Challenge.name, schema: ChallengeSchema },
     ]),
+    PlayersModule,
   ],
   controllers: [ChallengesController],
   providers: [ChallengesService, ChallengesRepository],
